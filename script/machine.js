2a4bd421d7363c1873233844ec8aa100d151d446
unknown <youremail@example.com>
2026-02-20 15:32:07 +0600

    all div added

--- script/machine.js
+++ script/machine.js
@@ -2,10 +2,16 @@ function showonly(id){
 const addmoney = document.getElementById("addmoneyy");
 const cashout = document.getElementById("cashout"); 
 const history = document.getElementById("history");
+const transfermoney = document.getElementById("transfer-money");
+const bonus = document.getElementById("bonus");
+const paybill = document.getElementById("pay-bill");
 //hiding all the section 
 addmoney.classList.add("hidden");
 cashout.classList.add("hidden");
 history.classList.add("hidden");
+transfermoney.classList.add("hidden");
+paybill.classList.add("hidden");
+bonus.classList.add("hidden");
 
 //id wala element take show korbo 
 const selectedsection = document.getElementById(id);
